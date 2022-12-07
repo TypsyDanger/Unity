beb820253dadd77a2a617a72279df3c04894f572
Tim Perry <tim.perry.wagner@gmail.com>
2022-12-07 15:43:23 -0400

    Initial Commit of the Design Patterns folder, complete with a working Command pattern project

---
+++ DesignPatterns/Command Pattern/Assets/Scripts/Control/MoveRight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Control
+{
+    public class MoveLeft : Command
+    {
+        public override void Execute(Transform playerTransform)
+        {
+            Move(playerTransform);
+        }
+
+        private override void Move(Transform playerTransform)
+        {
+            playerTransform.Translate(playerTransform.forward * moveDistance);
+        }
+    } 
+}
---
+++ DesignPatterns/Command Pattern/Assets/Scripts/Control/MoveUp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Control
+{
+    public class MoveRight : Command
+    {
+        public override void Execute(Transform playerTransform)
+        {
+            Move(playerTransform);
+        }
+
+        public override void Move(Transform playerTransform)
+        {
+            playerTransform.Translate(playerTransform.right * moveDistance);
+        }
+    } 
+}
