14e9b898a76d1a7ba1bf0f72ae2364cf259e5dfa
Tim Perry <tim.perry.wagner@gmail.com>
2022-12-13 22:12:54 -0400

    Added PlayFabIntegration folder and updated gitignore

---
+++ PlayfabIntegration/Assets/Scripts/Constants/PlayfabConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Constants
+{
+    public static class PlayfabConstants
+    {
+        public const string PLAYFAB_TITLE_ID = "74160";
+    }
+}
---
+++ PlayfabIntegration/Assets/Scripts/Utils/GetUniqueIdentifier.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Device;
+
+namespace Utils
+{
+    public static class GetUniqueIdentifier
+    {
+        public static string generate()
+        {
+            string uniqueId = "";
+            
+            #if UNITY_EDITOR
+            uniqueId = SystemInfo.deviceUniqueIdentifier + "_unity-ide";
+            #endif
+
+            return uniqueId;
+        }
+    }
+}
