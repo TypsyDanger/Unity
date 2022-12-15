0fe1e67812aaff35899787f5268e93c0acdec0d1
Tim Perry <tim.perry.wagner@gmail.com>
2022-12-15 16:58:07 -0400

    Updating PlayFabManager.cs

--- PlayfabIntegration/Assets/Scripts/Managers/PlayfabManager.cs
+++ PlayfabIntegration/Assets/Scripts/Managers/PlayfabManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PlayFabHandlers;
+using PlayFabScripts.Handlers;
 
 namespace Managers
 {
@@ -9,9 +9,24 @@ namespace Managers
     {
         public void Awake()
         {
-            PlayFabLoginHandler PlayFabLogin = new PlayFabLoginHandler();
+            StartCoroutine(RunTests());
+        }
 
+        public IEnumerator RunTests()
+        {
+            Debug.Log("Attempting Login...");
+            PlayFabLoginHandler PlayFabLogin = new PlayFabLoginHandler();
             PlayFabLogin.DoLogin();
+            yield return new WaitWhile(() => PlayFabPlayerProfile.IsLoggedIn == false);
+            
+            Debug.Log("Attempting to get player title data...");
+            PlayFabPlayerTitleDataHandler PlayFabDataHandler = new PlayFabPlayerTitleDataHandler();
+            PlayFabDataHandler.GetData();
+            yield return new WaitWhile(() => PlayFabPlayerProfile.TitleData == null);
+            
+            Debug.Log(string.Format("TitleData 'lastLogin': {0}", PlayFabPlayerProfile.TitleData.Find(x => x.Key == "lastLogin").Value));
+            
+            Debug.Log("All tests complete");
         }
     }
-}F
+}
